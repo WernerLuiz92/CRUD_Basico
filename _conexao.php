ed8e5f12b35fba7d89bd3986aa46f3ef5af53bcb
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-25 22:43:46 -0300

    Seção 3 - Aula 11
    - Criamos o nosso banco de dados, nossa primeira tabela.
    - Criamos um arquivo PHP para realizar a conexão ao banco de dados.
    - Alterarmos nosso arquivo de inserção dos dados para conectar ao banco de dados e inserir os dados.

---
+++ _conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost"; //Padrão - Server Local
+$database = "curso_estoque"; //Alterar conformew o nome do banco de dados
+$username = "root"; //Padrão - root
+$password = ""; // Senha do banco de dados
+
+// Criando a conexão
+$conexao = mysqli_connect($servername, $username, $password, $database);
+?>
--- _inserir_produto.php
+++ _inserir_produto.php
@@ -1,9 +1,22 @@
 <?php
+include '_conexao.php';
+
     $codprod = $_POST['codprod'];
     $descprod = $_POST['descprod'];
     $catprod = $_POST['catprod'];
     $qtdprod = $_POST['qtdprod'];
     $fornprod = $_POST['fornprod'];
 
-    echo $codprod, $descprod, $catprod, $qtdprod, $fornprod;
+    $sql = "INSERT INTO `produto`(`cod_prod`, `desc_prod`, `cat_prod`, `qtd_prod`, `forn_prod`) VALUES ($codprod,'$descprod','$catprod',$qtdprod,'$fornprod')";
+
+    $inserir = mysqli_query($conexao, $sql);
 ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<center>
+    <div class="container" style="width: 500px; margin-top: 40px;">
+        <h4>Produto cadastrado com sucesso!</h4>
+    </div>
+    <div style="padding-top: 20px">
+        <a href="index.php" role="button" class="btn btn-sm btn-primary">Voltar</a>
+    </div>
+</center>
