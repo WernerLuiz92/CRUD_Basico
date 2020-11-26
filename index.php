4588ef901de909bdea1c572c8b6d7783e888fd24
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-25 22:36:09 -0300

    Seção 3 - Aula 8
    Criamos um formulário para a inserção de dados.

--- index.php
+++ index.php
@@ -3,22 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap Instalado Localmente</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Formulário de Cadastro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <style>
+        #tamanhoContainer {
+            width: 500px;
+        }
+    </style>
 </head>
 <body>
+<div class="container" id="tamanhoContainer" style="margin-top: 40px;">
+    <form action="cadastro" method="post">
+        <div class="form-group">
+            <label>Cód. Produto</label>
+            <input type="number" class="form-control" placeholder="Insira o código do produto">
+        </div><!--Fim form-group -->
+        <div class="form-group">
+            <label>Descrição</label>
+            <input type="text" class="form-control" placeholder="Insira o nome do produto">
+        </div><!--Fim form-group -->
+        <div class="form-group">
+            <label>Categoria</label>
+            <select class="form-control">
+                <option>Periféricos</option>
+                <option>Hardware</option>
+                <option>Software</option>
+                <option>Smartphones</option>
+                <option>Gadgets</option>
+            </select>
+        </div><!--Fim form-group -->
+        <div class="form-group">
+            <label>Quantidade</label>
+            <input type="number" class="form-control" placeholder="Insira a quantidade">
+        </div><!--Fim form-group -->
+        <div class="form-group">
+            <label>Fornecedor</label>
+            <select class="form-control">
+                <option>Pauta</option>
+                <option>Braile</option>
+                <option>Aldo</option>
+                <option>Solid</option>
+            </select>
+        </div><!--Fim form-group -->
+        <button type="submit" class="btn btn-success btn-sm">Salvar</button>
+        <button type="submit" class="btn btn-danger btn-sm">Limpar</button>
+    </form>
+</div><!--Fim container -->
 
-<button type="button" class="btn btn-primary">Teste 1</button>
-<button type="button" class="btn btn-secondary">Teste 2</button>
-<button type="button" class="btn btn-success">Teste 3</button>
-<button type="button" class="btn btn-danger">Teste 4</button>
-<button type="button" class="btn btn-warning">Teste 5</button>
-<button type="button" class="btn btn-info">Teste 6</button>
-<button type="button" class="btn btn-light">Teste 7</button>
-<button type="button" class="btn btn-dark">Teste 8</button>
-
-<button type="button" class="btn btn-link">Teste 9</button>
-
- <script type="text/javascript" src="js/bootstrap.js"></script>   
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
