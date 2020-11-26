636b0ae5405461b90aec240902b5f409e091c3bc
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2020-11-25 22:45:41 -0300

    Seção 4 - Aula 12
    - Criamos um novo arquivo para listar os produtos cadastrados.
    - Alteramos algumas coisas no index.php

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -11,7 +11,8 @@ include '_conexao.php';
 
     $inserir = mysqli_query($conexao, $sql);
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <center>
     <div class="container" style="width: 500px; margin-top: 40px;">
         <h4>Produto cadastrado com sucesso!</h4>
--- index.php
+++ index.php
@@ -1,71 +1,90 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Cadastro</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <style>
-        #tamanhoContainer {
-            width: 500px;
-        }
+    #tamanhoContainer {
+        width: 500px;
+    }
 
-        #botao1 {
-            background-color: #5A8D2F;
-            color: #FFFFFF;
-        }
-        #botao2 {
-            background-color: #BB1D03;
-            color: #FFFFFF;
-        }
+    #botao1 {
+        background-color: #5A8D2F;
+        color: #FFFFFF;
+    }
 
+    #botao2 {
+        background-color: #BB1D03;
+        color: #FFFFFF;
+    }
     </style>
 </head>
+
 <body>
-<div class="container" id="tamanhoContainer" style="margin-top: 40px;">
-    <h4>Cadastro de Produtos</h4>
-    <form action="_inserir_produto.php" method="post" style="margin-top: 20px;">
-        <div class="form-group">
-            <label>Cód. Produto</label>
-            <input type="number" class="form-control" name="codprod" placeholder="Insira o código do produto" required>
-        </div><!--Fim form-group -->
-        <div class="form-group">
-            <label>Descrição</label>
-            <input type="text" class="form-control" name="descprod" placeholder="Insira o nome do produto" autocomplete="off">
-        </div><!--Fim form-group -->
-        <div class="form-group">
-            <label>Categoria</label>
-            <select class="form-control" name="catprod">
-                <option>Periféricos</option>
-                <option>Hardware</option>
-                <option>Software</option>
-                <option>Smartphones</option>
-                <option>Gadgets</option>
-            </select>
-        </div><!--Fim form-group -->
-        <div class="form-group">
-            <label>Quantidade</label>
-            <input type="number" class="form-control" name="qtdprod" placeholder="Insira a quantidade">
-        </div><!--Fim form-group -->
-        <div class="form-group">
-            <label>Fornecedor</label>
-            <select class="form-control" name="fornprod">
-                <option>Pauta</option>
-                <option>Braile</option>
-                <option>Aldo</option>
-                <option>Solid</option>
-            </select>
-        </div><!--Fim form-group -->
-        <div class="botoes" style="text-align: right;">
-            <button type="submit" id="botao1" class="btn btn-sm">Salvar</button>
-            <button type="submit" id="botao2" class="btn btn-sm">Limpar</button>
-        </div><!--Fim botoes -->
-        
-    </form>
-</div><!--Fim container -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <div class="container" id="tamanhoContainer" style="margin-top: 40px;">
+        <h4>Cadastro de Produtos</h4>
+        <form action="_inserir_produto.php" method="post" style="margin-top: 20px;">
+            <div class="form-group">
+                <label>Cód. Produto</label>
+                <input type="number" class="form-control" name="codprod" placeholder="Insira o código do produto"
+                    required>
+            </div>
+            <!--Fim form-group -->
+            <div class="form-group">
+                <label>Descrição</label>
+                <input type="text" class="form-control" name="descprod" placeholder="Insira o nome do produto"
+                    autocomplete="off">
+            </div>
+            <!--Fim form-group -->
+            <div class="form-group">
+                <label>Categoria</label>
+                <select class="form-control" name="catprod">
+                    <option>Periféricos</option>
+                    <option>Hardware</option>
+                    <option>Software</option>
+                    <option>Smartphones</option>
+                    <option>Gadgets</option>
+                </select>
+            </div>
+            <!--Fim form-group -->
+            <div class="form-group">
+                <label>Quantidade</label>
+                <input type="number" class="form-control" name="qtdprod" placeholder="Insira a quantidade">
+            </div>
+            <!--Fim form-group -->
+            <div class="form-group">
+                <label>Fornecedor</label>
+                <select class="form-control" name="fornprod">
+                    <option>Pauta</option>
+                    <option>Braile</option>
+                    <option>Aldo</option>
+                    <option>Solid</option>
+                </select>
+            </div>
+            <!--Fim form-group -->
+            <div class="botoes" style="text-align: right;">
+                <button type="submit" id="botao1" class="btn btn-sm">Salvar</button>
+                <button type="submit" id="botao2" class="btn btn-sm">Limpar</button>
+            </div>
+            <!--Fim botoes -->
+
+        </form>
+    </div>
+    <!--Fim container -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
